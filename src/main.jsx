1af309f313819c9bf4b4996e211ec6f657ccd422
Reddaiah799 <168219528+Reddaiah799@users.noreply.github.com>
2025-10-10 12:25:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  // <--- Add this
 import { AdminLogin } from './components/TravelExplore/admin-login.jsx';
 import { AdminDashboard } from './components/TravelExplore/admin-dashboard.jsx';
 import { UserLogin } from './components/TravelExplore/user-login.jsx';
@@ -11,21 +12,18 @@ import { UserRegister } from './components/TravelExplore/user-register.jsx';
 import { UserDashboard } from './components/TravelExplore/user-dashboard.jsx';
 import { PlaceDetails } from './components/TravelExplore/PlaceDetails.jsx';
 
-
 createRoot(document.getElementById('root')).render(
-  
-    <StrictMode>
+  <StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<AdminLogin/>}/>
-       <Route path="/admin-dashboard" element={<AdminDashboard />} />
-       <Route path="/user-login" element={<UserLogin/>}/>
-       <Route path="/admin-login" element={<AdminLogin/>}/>
-       <Route path="/user-register" element={<UserRegister/>} />
-       <Route path="/user-dashboard" element={<UserDashboard/>}/>
-       <Route path="/placeDetails/:id" element={<PlaceDetails />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<AdminLogin />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/user-login" element={<UserLogin />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/user-register" element={<UserRegister />} />
+        <Route path="/user-dashboard" element={<UserDashboard />} />
+        <Route path="/placeDetails/:id" element={<PlaceDetails />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
-
 );
